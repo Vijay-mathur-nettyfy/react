b7a205c1fb3a8cb37ee3a0e13b1a8b3b01973a05
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-17 18:32:47 +0530

    react application

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Nettyfy Family, Welcome to all of you.</h1>
+        <h3>
+          In this technical session, we will see how to deploy react and node js
+          based applications.
+        </h3>
+        <h2>Let's start.</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
