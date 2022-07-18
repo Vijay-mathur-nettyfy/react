7bfdd685dfb1996585d9ecf1edb2e5470b26fd3f
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-18 23:29:44 +0530

    once again modified the powershell scripts

--- push.ps1
+++ push.ps1
@@ -9,12 +9,12 @@ function gitPush($message){
  git add . 
  git commit -m $message 
  git push origin  (git branch --show-current )
- echo "----------Code is pushed successfully----------"
+ echo "----------Code is pushed successfully----------\n"
 
- echo "----------Build is Generating----------"
+ echo "----------Build is Generating----------\n"
  gitDeploy
- echo "----------Build is Generated----------"
- echo "----------React Application is deployd successfully----------"
+ echo "----------Build is Generated----------\n"
+ echo "----------React Application is deployd successfully----------\n"
 }
 
   gitPush -message $args[0]
