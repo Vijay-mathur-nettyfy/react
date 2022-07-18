71601fa155d27a5ae69e165025eaaab6c94cc796
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-18 23:30:26 +0530

    once more

--- push.ps1
+++ push.ps1
@@ -9,12 +9,19 @@ function gitPush($message){
  git add . 
  git commit -m $message 
  git push origin  (git branch --show-current )
- echo "----------Code is pushed successfully----------\n"
+ echo "----------Code is pushed successfully----------"
+ echo ""
 
- echo "----------Build is Generating----------\n"
+ echo "----------Build is Generating----------"
+ echo ""
+
  gitDeploy
- echo "----------Build is Generated----------\n"
- echo "----------React Application is deployd successfully----------\n"
+ echo "----------Build is Generated----------"
+ echo ""
+
+ echo "----------React Application is deployd successfully----------"
+ echo ""
+
 }
 
   gitPush -message $args[0]
