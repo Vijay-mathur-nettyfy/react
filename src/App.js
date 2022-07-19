b28e007deaa7cae935da8d83edf802cac6b32c85
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-20 01:23:45 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -6,28 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello Nettyfy Family, Welcome to all of you.</h1>
-        <h3>
-          In this technical session, we will see how to deploy react and node js
-          based applications.
-        </h3>
-        <h2>
-          In this case we have deployed our react application from github
-          through CI/CD
-        </h2>
-        <p>This is comming from Private Repo</p>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>We are going to deploy our react application</h2>
       </header>
     </div>
   );
