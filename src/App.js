a78a30a20c919f6090222925cf4f8475097acab1
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-20 01:45:20 +0530

    CI/CD also implemented

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello Nettyfy Family, Welcome to all of you.</h1>
-        <h2> We are going to deploy our react application</h2>
+        <h2> We are going to deploy our react application </h2>
+        <h3> We have also implemented CI/CD</h3>
       </header>
     </div>
   );
