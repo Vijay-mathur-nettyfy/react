7c2e4561ec8d2eff94f797808e43d328223b755c
vijaymathur <vijaymathur.nettyfy@gmail.com>
2022-07-20 17:18:06 +0530

    added ci/cd pipeline

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <h1>Hello Nettyfy Family, Welcome to all of you.</h1>
         <h2> We are going to deploy our react application </h2>
         <h3> We have also implemented CI/CD</h3>
+        <h4>CI/CD Pipeline is added</h4>
       </header>
     </div>
   );
